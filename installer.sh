8ef76e9347eb7775b4301e1d2aee4e26f934f1e7
Albaro Pereyra <AlbaroPereyra22@gmail.com>
2024-06-14 14:54:40 -0700

    updated install script

--- installer.sh
+++ installer.sh
@@ -17,6 +17,7 @@ git clone $repoUrl ~/${fileName};
 # Creates a directory if it does not exist
 mkdir -p ~/.emacs.d;
 touch ~/.emacs.d/.emacs-custom.el;
-mv ~/${fileName}/init.el ~/.emacs.d;
+# TODO update based on uname
+mv ~/${fileName}/WSL2Init.el ~/.emacs.d;
 rm -rf ~/${fileName};
 printf "Emacs conf has been updated.\n";
