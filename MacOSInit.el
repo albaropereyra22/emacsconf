4bc8664da5d0147389dbe1e20b56e0340192d958
Albaro Pereyra <141711575+albaropereyra22@users.noreply.github.com>
2025-06-12 03:33:35 -0700

    Update MacOSInit.el allow killing and yanking.

--- MacOSInit.el
+++ MacOSInit.el
@@ -193,15 +193,15 @@
 
 ;;; MAC OS settings ;;;
 ;;Allow Killing and Yanking on MacOS clipboard
-; (defun copy-from-osx ()
-;   (shell-command-to-string "pbpaste"))
-; (defun paste-to-osx (text &optional push)
-;   (let ((process-connection-type nil))
-;     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-;       (process-send-string proc text)
-;       (process-send-eof proc))))
-;
-; (setq interprogram-cut-function 'paste-to-osx)
-; (setq interprogram-paste-function 'copy-from-osx)
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
 ;;; Windows settings
 ;(normal-erase-is-backspace-mode 1)
